3b1ed603960515d09a8c7e6fa64cd87828698b21
Carlyle Oliver <carlyleoliver@gmail.com>
2017-09-25 20:51:57 +0530

    Added base plugin file.

---
+++ smarterleads.php
@@ -0,0 +1,24 @@
+<?php
+	/*
+	Plugin Name: Smarter Leads
+	Plugin URI:  http://pseudokool.github.io/wp_smarterleads/
+	Description: A Wordpress plugin, that add a CRM-like functionality to leads generated.
+	Version:     1.0a
+	Author:      pseudokool
+	Author URI:  https://github.com/pseudokool
+
+	Smarter Leads is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	any later version.
+	 
+	Smarter Leads is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+	 
+	You should have received a copy of the GNU General Public License
+	along with Newstream. If not, see {License URI}.
+	*/
+
+?>
