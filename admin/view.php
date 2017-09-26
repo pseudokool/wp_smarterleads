e54b7bf25b7bdf7493ed7705013cd5657e08702f
Carlyle Oliver <carlyleoliver@gmail.com>
2017-09-26 17:36:57 +0530

    Added lead search

--- admin/view.php
+++ admin/view.php
@@ -31,5 +31,9 @@ $tbl_leads->prepare_items();
 ?>	
 <div class="wrap">
 	<h1><?= esc_html(get_admin_page_title()); ?></h1>
+  <form method="post">
+	  <input type="hidden" name="page" value="tbl_leads" />
+	  <?php $tbl_leads->search_box('search', 'search_id'); ?>
+	</form>
 	<?php $tbl_leads->display(); ?>
 </div>
